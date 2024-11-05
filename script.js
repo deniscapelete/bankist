e3acb42c66368a9d88cd04dbaab3b792444103a3
Denis <capelete.denis@hotmail.com>
2024-11-04 21:36:11 -0300

    193. Event Delegation: Implementing Page Navigation

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -34,9 +39,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
+// button scrolling
 /* -------------------- 189. Implementing Smooth Scrolling -------------------- */
 btnScrollTo.addEventListener('click', function (e) {
   /* 
@@ -44,8 +48,26 @@ btnScrollTo.addEventListener('click', function (e) {
 
    O método `scrollIntoView` automaticamente calcula a posição da seção no documento e rola a página até ela. 
    Ao passar `{ behavior: 'smooth' }` como parâmetro, a rolagem é feita de forma suave, assim como no exemplo anterior. 
-   
+
    Essa abordagem é mais simples e recomendada para rolar até um elemento específico, já que não requer cálculos manuais.
 */
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// Page navigation
+// 1. Add Event Listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
 });
